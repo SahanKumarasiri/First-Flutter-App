8dbf1f5c52b1ee67ca3245fcc07bd1b0a04d4c2f
SahanKumarasiri <kumarasirisahan@gmail.com>
2021-11-29 19:41:04 +0530

    TabBar implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 main() {
   runApp(MyApp());
@@ -9,34 +10,66 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text("Amaya Bookshop"),
-            leading: IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {},
-            ),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.search),
+      home: DefaultTabController(
+        length: 3,
+        child: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                "අමායා පොත් මැදුර",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    foreground: Paint()
+                      ..shader = ui.Gradient.linear(
+                        const Offset(0, 20),
+                        const Offset(150, 20),
+                        <Color>[
+                          Colors.red,
+                          Colors.yellow,
+                        ],
+                      )),
+              ),
+              leading: IconButton(
+                icon: const Icon(Icons.menu),
                 onPressed: () {},
               ),
-              IconButton(
-                icon: const Icon(Icons.more_vert),
-                onPressed: () {},
-              )
-            ],
-            flexibleSpace: Image.asset(
-              "assets/logo.jpg",
-              fit: BoxFit.cover,
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.more_vert),
+                  onPressed: () {},
+                )
+              ],
+              flexibleSpace: Image.asset(
+                "assets/logo.jpg",
+                fit: BoxFit.cover,
+              ),
+              bottom: const TabBar(
+                tabs: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+                    child: Text("Novel"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+                    child: Text("Adventure"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+                    child: Text("Programming"),
+                  ),
+                ],
+              ),
             ),
-          ),
-          body: const Center(
-            child: Text(
-              "Hello World",
-              style: TextStyle(
-                  fontSize: 20, color: Colors.red, fontWeight: FontWeight.bold),
+            body: const TabBarView(
+              children: [
+                Icon(Icons.directions_car),
+                Icon(Icons.directions_transit),
+                Icon(Icons.directions_bike),
+              ],
             ),
           ),
         ),
